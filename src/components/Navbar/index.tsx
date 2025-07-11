7968a8608febd6222bbb7b2c0feaa065109a7321
Vijay Khot <vijaysinh.khot@gmail.com>
2025-07-11 11:18:13 -0400

    Updated Resume

--- src/components/Navbar/index.tsx
+++ src/components/Navbar/index.tsx
@@ -53,7 +53,7 @@ const Navbar = () => {
             {resumeOpen && (
               <div className="absolute right-0 z-20 w-48 py-2 mt-2 text-sm text-white bg-gray-800 rounded-lg shadow-lg">
                 <a
-                  href="/resume/Vijay_Khot_Resume_full_stack.pdf"
+                  href="/resume/Vijay_Khot_Resume.pdf"
                   target="_blank"
                   rel="noopener noreferrer"
                   className="block px-4 py-2 hover:bg-gray-700"

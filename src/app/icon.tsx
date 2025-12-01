cca3ce942cfe6d3c0c783a224daf16a267a44512
Vijay Khot <vijaysinh.khot@gmail.com>
2025-11-30 20:16:48 -0500

    feat(icon): add custom icon generation using ImageResponse

---
+++ src/app/icon.tsx
@@ -0,0 +1,36 @@
+import { ImageResponse } from "next/og";
+
+// Image metadata
+export const size = {
+  width: 32,
+  height: 32,
+};
+export const contentType = "image/png";
+
+// Image generation
+export default function Icon() {
+  return new ImageResponse(
+    (
+      // ImageResponse JSX element
+      <div
+        style={{
+          fontSize: 20,
+          background: "#3b82f6",
+          width: "100%",
+          height: "100%",
+          display: "flex",
+          alignItems: "center",
+          justifyContent: "center",
+          color: "white",
+          borderRadius: "10px",
+          fontWeight: 900,
+        }}
+      >
+        VK
+      </div>
+    ),
+    {
+      ...size,
+    }
+  );
+}
